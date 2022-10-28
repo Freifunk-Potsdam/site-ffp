58f65ce02b5e5882e059421f78897d2b50050294
Seth0r <github@seth0r.net>
2022-10-28 17:00:15 +0200

    added buildscript for gluon

---
+++ build_gluon.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+GLUON_RELEASE=2022.10
+OPENWRT_VERSION=22.03.2
+TARGETS="x86-generic ath79-generic ath79-nand"
+
+CPUS=`grep -c ^processor /proc/cpuinfo`
+cd `dirname $0`/..
+
+echo "Cleaning Output..."
+rm -rf output
+echo "Updating Modules..."
+make update
+for T in $TARGETS; do
+    echo "Building Target $T..."
+    make GLUON_TARGET=$T GLUON_RELEASE=$GLUON_RELEASE CONFIG_VERSIONOPT=y CONFIG_VERSION_NUMBER=$OPENWRT_VERSION -j$CPUS
+done
