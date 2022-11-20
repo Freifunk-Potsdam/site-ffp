832875fe2dc44a4847c9f5973c6a398d52c0f2b5
seth0r <github@seth0r.net>
2022-11-20 16:55:33 +0100

    being more chatty

--- build_gluon.sh
+++ build_gluon.sh
@@ -5,7 +5,17 @@ DEFAULT_TARGETS="x86-generic ath79-generic ath79-nand ramips-mt76x8"
 GLUON_RELEASE="${GLUON_RELEASE:-`date +%Y.%m.%d`}"
 TARGETS="${TARGETS:-$DEFAULT_TARGETS}"
 CPUS=`grep -c ^processor /proc/cpuinfo`
+
 cd `dirname $0`/..
+
+if [ "$1" != "" ]; then
+    echo "Building Gluon Release ${GLUON_RELEASE} (${1}) for targets: ${TARGETS}..."
+    if [ ! -f "$2" ]; then
+        echo "Manifest will not be signed, because no secret was given."
+    fi
+else
+    echo "Building Gluon Dev-Release ${GLUON_RELEASE} for targets: ${TARGETS}..."
+fi
 
 echo "Cleaning Output..."
 rm -rf output
