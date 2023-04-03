8755fb5f748db68c1219dc170c702ad2d191b93c
Seth0r <github@seth0r.net>
2023-04-03 12:55:10 +0200

    generating different version numbers for testing,early and stable builds

--- build_gluon.sh
+++ build_gluon.sh
@@ -2,18 +2,36 @@
 OPENWRT_VERSION=22.03.2
 DEFAULT_TARGETS="x86-generic ath79-generic ath79-nand ramips-mt76x8"
 
-GLUON_RELEASE="${GLUON_RELEASE:-`date +%Y.%m.%d`}"
 TARGETS="${TARGETS:-$DEFAULT_TARGETS}"
 CPUS=`grep -c ^processor /proc/cpuinfo`
 
 cd `dirname $0`/..
 
 if [ "$1" != "" ]; then
+    if [ "$1" == "testing" ]; then
+        GLUON_RELEASE="${GLUON_RELEASE:-`date +%Y.%m.%d`}"
+    elif [ "$1" == "early" ]; then
+        YEAR=`date +%Y`
+        LASTVER=`git -C site tag -l | grep $YEAR | cut -d. -f2`
+        if [ "$LASTVER" == "" ]; then
+            GLUON_RELEASE="${GLUON_RELEASE:-${YEAR}.1}-early"
+        else
+            GLUON_RELEASE="${GLUON_RELEASE:-${YEAR}.$(( $LASTVER + 1 ))}-early"
+        fi
+    elif [ "$1" == "stable" ]; then
+        TAG=`git -C site describe --abbrev=0 --tags`
+        GLUON_RELEASE="$TAG-stable"
+        if [ "`git -C site log ${TAG}..HEAD`" != "" ]; then
+            echo "Changes since last tag: ${TAG}"
+            exit
+        fi
+    fi
     echo "Building Gluon Release ${GLUON_RELEASE} (${1}) for targets: ${TARGETS}..."
     if [ ! -f "$2" ]; then
         echo "Manifest will not be signed, because no secret was given."
     fi
 else
+    GLUON_RELEASE="${GLUON_RELEASE:-`date +%Y.%m.%d`}-dev"
     echo "Building Gluon Dev-Release ${GLUON_RELEASE} for targets: ${TARGETS}..."
 fi
 
@@ -52,7 +70,12 @@ if [ "$1" != "" ]; then
     (
     git -C site remote get-url origin
     git -C site diff -s && git -C site status -sb -uall
-    git -C site log `git -C site describe --abbrev=0 --tags`..HEAD
+    if [ "$2" == "stable" ]; then
+        LASTTAG=`git -C site tag -l | sort | grep -B1 ${TAG} | head -n1`
+        git -C site log ${LASTTAG}..${TAG}
+    else
+        git -C site log `git -C site describe --abbrev=0 --tags`..HEAD
+    fi
     ) > output/images/${GLUON_RELEASE}.changes
 fi
 echo "done."
