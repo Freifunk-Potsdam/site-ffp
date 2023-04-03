81a6cdddca7f7d84d33a6b2719c1eccdcfb8d8ec
Seth0r <github@seth0r.net>
2023-04-03 22:13:24 +0200

    added some more strict shell settings

--- build_vars.sh
+++ build_vars.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# check and abort on unbound vars
+set -o nounset
+# show errors in pipes
+set -o pipefail
+
 OPENWRT_VERSION=22.03.2
 DEFAULT_TARGETS="x86-generic ath79-generic ath79-nand ramips-mt76x8"
 
