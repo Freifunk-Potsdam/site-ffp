ee83e98232cf030bbe1ffbe9ce0cbda1950016c2
Seth0r <github@seth0r.net>
2023-06-12 18:07:52 +0200

    building ramips-mt7620 and ramips-mt7621 targets

--- build_vars.sh
+++ build_vars.sh
@@ -6,7 +6,7 @@ set -o nounset
 set -o pipefail
 
 OPENWRT_VERSION=22.03.2
-DEFAULT_TARGETS="x86-generic ath79-generic ath79-nand ramips-mt76x8"
+DEFAULT_TARGETS="x86-generic ath79-generic ath79-nand ramips-mt76x8 ramips-mt7620 ramips-mt7621"
 
 TARGETS="${TARGETS:-$DEFAULT_TARGETS}"
 CPUS=$(nproc)
