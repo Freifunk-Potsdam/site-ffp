49985667164b58a06f7be993878697333b1a2507
Hannes Fuchs <hannes.fuchs@gmx.org>
2023-08-06 21:02:53 +0200

    Adjust OpenWrt version
    Adjust the OpenWrt verson to match the used one in gluon.

--- build_vars.sh
+++ build_vars.sh
@@ -5,7 +5,7 @@ set -o nounset
 # show errors in pipes
 set -o pipefail
 
-OPENWRT_VERSION=22.03.2
+OPENWRT_VERSION=22.03
 DEFAULT_TARGETS="x86-generic ath79-generic ath79-nand ramips-mt76x8 ramips-mt7621"
 
 TARGETS="${TARGETS:-$DEFAULT_TARGETS}"
