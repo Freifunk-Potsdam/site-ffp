c94e4c3fc26af2404fe6994fd42179c1322a8b28
Hannes Fuchs <hannes.fuchs@gmx.org>
2024-01-22 21:53:06 +0100

    Update OpenWrt version in vars
    The OpenWrt version is used to point to the package repository. The used
    used version ist defined by the commit hash in `gluon/modules`. Also see
    in `openwrt/include/version.mk` of the specified hash in the OpenWrt
    repository.
    
    If not specified corectly the `opkg` command will fail to fetch
    packages not provided by ffp.

--- build_vars.sh
+++ build_vars.sh
@@ -5,7 +5,7 @@ set -o nounset
 # show errors in pipes
 set -o pipefail
 
-OPENWRT_VERSION=22.03
+OPENWRT_VERSION=23.05.2
 DEFAULT_TARGETS="x86-generic ath79-generic ath79-nand ramips-mt76x8 ramips-mt7621"
 
 TARGETS="${TARGETS:-$DEFAULT_TARGETS}"
