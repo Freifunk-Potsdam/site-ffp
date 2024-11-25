e48ea3acce9c48e7d7a788dee7e858f01ea2363d
Seth0r <github@seth0r.net>
2024-11-25 10:32:47 +0100

    adding mt7915-hotfix to all affected devices
    https://github.com/ffac/site/blob/v2023.2.x/image-customization.lua#L210-L215
    https://gitlab.freifunk-stuttgart.de/firmware/site-ffs/-/blob/v3.1/image-customization.lua#L185-L196

--- image-customization.lua
+++ image-customization.lua
@@ -193,6 +193,12 @@ if device({'zyxel-nwa55axe'}) then
     broken(false)
     packages({
         'ffda-network-setup-mode',
-        'ffac-mt7915-hotfix',
     })
+end
+
+if target('ramips', 'mt7621') or target('ramips', 'mt7622') or target('mediatek', 'filogic') then
+    -- restart device if mt7915e driver shows known failure symptom
+    packages {
+        'ffac-mt7915-hotfix',
+    }
 end
